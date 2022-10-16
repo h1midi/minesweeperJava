839805df0ac7dfb5834de6f55ca9eafe4e87f11a
hamidi <hammiddi@users.noreply.github.com>
2022-10-16 17:33:46 +0100

    final commit

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -3,8 +3,10 @@ package org.example;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.text.AbstractDocument.Content;
+
 import java.awt.event.*;
-import java.util.*;
+import java.util.ArrayList;
 
 public class Game extends JFrame implements ActionListener, MouseListener{
 
@@ -14,9 +16,10 @@ public class Game extends JFrame implements ActionListener, MouseListener{
     JPanel buttonPanel = new JPanel();
     Container grid = new Container();
     int[][] counts;
+    
     JButton[][] buttons;
     int size;
-    static final int MINE = 10;
+    static final int MINE = 9;
 
 
     public Game(int size) {
@@ -24,6 +27,7 @@ public class Game extends JFrame implements ActionListener, MouseListener{
 
         this.size = size;
         counts = new int[size][size];
+
         buttons = new JButton[size][size];
 
         frame.setSize(900,900);
@@ -44,16 +48,86 @@ public class Game extends JFrame implements ActionListener, MouseListener{
             }
         }
 
+        
         buttonPanel.add(reset);
         buttonPanel.add(giveUp);
 
         frame.add(grid,BorderLayout.CENTER);
-        // make mines
+        createMines(size);
+                    creatNums(size);
 
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         frame.setVisible(true);
 
 
+
+    }
+
+    public void createMines(int s){
+        ArrayList<Integer> list = new ArrayList<>();
+                for(int x = 0; x < s; x++)
+                {
+                    for(int y = 0; y < s; y++)
+                    {
+                        list.add(x*100+y);
+                    }
+                }
+                counts = new int[s][s];
+                for(int a = 0; a < (int)(s * 1.5); a++)
+                        {
+                            int choice = (int)(Math.random() * list.size());
+                            counts [list.get(choice) / 100] [list.get(choice) % 100] = MINE;
+                            list.remove(choice);
+                        }
+        }
+
+         public void takeTheL(int m){
+
+                for(int x = 0; x < size; x++)
+                {
+                    for(int y = 0; y < size; y++)
+                    {
+                        if(buttons[x][y].isEnabled())  
+                        {
+                            if(counts[x][y] != MINE)
+                            {
+                                buttons[x][y].setText(""+ counts[x][y]);
+                            }
+
+                            else
+                            {
+                                buttons[x][y].setText("X");
+
+                            }
+                            buttons[x][y].setEnabled(false);
+                        }
+                    }
+                }
+                //boom
+            }
+
+    public Void creatNums(int size) {
+        int k=0;
+
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+
+                if(counts[i][j]!=9){
+                    int x;
+
+                    for(x=i-1 ; x<=i+1 ; x++){
+                        for(int y=j-1 ; y<=j+1 ; y++){
+                            if(counts[x][y]==MINE&&!(x<0||x>size-1||y<0||y>size-1)){
+                                k++;
+                            }
+                        }
+                    }
+                    counts[i][j]=k;
+                    k=0;
+                }
+            }
+        }
+        return null;
     }
 
     @Override
@@ -66,14 +140,18 @@ public class Game extends JFrame implements ActionListener, MouseListener{
                 {
                     buttons[x][y].setEnabled(true);
                     buttons[x][y].setText("");
-                    // make mines
+                    createMines(size);
+                    creatNums(size);
+                    creatNums(size);
                 }
             }
         }
+
         else if(ae.getSource() == giveUp)
         {
-             // loos
+             takeTheL(size);
         }
+
         else
         {
             for(int x = 0; x < size; x++)
@@ -82,7 +160,14 @@ public class Game extends JFrame implements ActionListener, MouseListener{
                 {
                     if(ae.getSource() == buttons[x][y])
                     {
-                        // click on a cell
+                        if(counts[x][y]==9)
+                            takeTheL(size);
+                        else
+                        {
+                            buttons[x][y].setEnabled(false);
+                            buttons[x][y].setText(""+counts[x][y]);
+
+                        }
                     }
                 }
             }
@@ -90,7 +175,8 @@ public class Game extends JFrame implements ActionListener, MouseListener{
 
 
     }
-        @Override
+    
+    @Override
     public void mouseClicked(MouseEvent me) {
         SwingUtilities.isRightMouseButton(me);
     }
